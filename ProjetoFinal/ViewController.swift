4c0c5710cfe3e29d1fdf0b7c3df75b450da9d195
Elisa Kalil <elisakalil@192.168.1.3>
2021-10-18 22:14:13 -0300

    commit arquivo cores
    Adicionado arquivo Color

--- ProjetoFinal/ViewController.swift
+++ ProjetoFinal/ViewController.swift
@@ -11,8 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = Colors.rosaClaro.uiColor
     }
+    
 
 
 }
