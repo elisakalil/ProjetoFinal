d245de02099ea149e7d4e3a370e1bb603552e0f7
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-21 15:58:28 -0300

    criação do layout da initialViewController

---
+++ ProjetoFinal/Controller/InitialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InitialViewController.swift
+//  ProjetoFinal
+//
+//  Created by Rayana Prata Neves on 21/10/21.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
