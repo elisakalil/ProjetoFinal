4caa65c2c098fcf8cfd1bbb2e0f7fcacd207c8e9
Elisa Kalil <elisakalilelias@gmail.com>
2021-10-28 11:34:24 -0300

    Reimplementação do enum de tipo de erros.

---
+++ ProjetoFinal/Models/Enums/SGApiError.swift
@@ -0,0 +1,16 @@
+//
+//  SGApiError.swift
+//  ProjetoFinal
+//
+//  Created by Elisa Kalil on 28/10/21.
+//
+
+import Foundation
+
+enum SGApiError {
+    case emptyResponse
+    case notFound
+    case emptyArray
+    case serverError
+    case invalidResponse
+}
